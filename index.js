7a8ec4e7d173ef25375ca1f56b97a16c187235b8
mkdoh3 <mkdoherty3@gmail.com>
2019-11-09 14:36:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-function writeCards( namesArray, event ) {
-  let thankYouCards = []
-  for ( let i = 0; i < namesArray.length; i++ ) {
-    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+function writeCards(namesArray, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouCards.push(
+      `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`
+    );
   }
-  return thankYouCards
+  return thankYouCards;
 }
 
-function countDown( startingNumber ) {
-  while ( startingNumber > 0 ) {
-    console.log( startingNumber );
+function countDown(startingNumber) {
+  while (startingNumber > 0) {
+    console.log(startingNumber);
     startingNumber -= 1;
   }
-  console.log( startingNumber );
+  console.log(startingNumber);
 }
